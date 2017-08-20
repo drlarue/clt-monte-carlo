8596e8013a1fe7db997874c2b57aa3ecf4668196
Daweon Ryu <daweon.ryu@gmail.com>
2017-08-20 14:10:14 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Exploring CLT via Monte Carlo Simulation
+
+For a single sample (consisting of `n` independent observations), the Central Limit Theorem (CLT) is typically invoked to argue that the sample mean approximately follows a Normal distribution.
+But exactly how good is this approximation? 
+
+To answer this question, we can simulate the frequentist thought experiment of repeated sampling.
+This can be achieved by Monte Carlo simulation, where samples are repeatedly drawn from a known parent population and the t-statistic is computed for each sample. 
+With this simulated sampling distribution of the t-statistics in hand, we can evaluate the properties of the sample mean estimator.
+
+
+This code provides a selection of methods to evaluate the Normal approximation for different population distributions and sample sizes.
+
+The `ExploreCLT` class has a `report` method that outputs the following:
+
+* Two plots:
+	1. The pdf (by KDE) of the sampling distribution of the t-statistics along with the theoretical t-distribution 
+		* The Kullback-Leibler divergence from the theoretical distribution to the sampling distribution is also calculated
+	2. The normal probability plot of the sample t-statistics
+* Actual test size `α` from the sampling distribution based on the critical values for the desired `α` (for a two-tailed test)
+
+
+## Example 
+
+Let's consider a case where the parent population is an exponential distribution, and the sample size is 50.
+
+```python
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-white')
+%matplotlib inline
+
+from clt_mc import ExploreCLT
+
+exponential = ExploreCLT(lambda n: np.random.normal(size=n))
+exponential.report(sample_size=50, desired_alpha=0.05)
+```
+
+This generates the following output:
+![example_plots](https://user-images.githubusercontent.com/26487650/29498411-a6db72aa-85b0-11e7-8d16-aef457bbd9d1.png)
+
+```
+Desired alpha for a two-tailed test: 0.05
+Actual alpha for a two-tailed test: 0.0628937106289
+```
+Based on the critical values for a desired `α` of 0.05, the actual `α` is 0.0629, which indicates a higher false positive rate. 
+The Normal probability plot shows that Normal approximation is poor at n=50.
---
+++ clt_mc.py
@@ -0,0 +1,127 @@
+import numpy as np
+import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+class ExploreCLT:
+    """
+    Selects the parent population from which samples will be drawn.
+    Example input: `lamba n: np.random.normal(size=n)`
+
+    Arguments:
+        * sampler: population distribution
+    """
+    def __init__(self, sampler):
+        self.sampler = sampler
+
+
+    def sampling_mean(self, sample_size, sim_reps=10000):
+        """
+        Repeatedly draws samples of size n from the population,
+        computing the t-statistic of each sample.
+        Returns t-statistics (array) and sample size n as a tuple.
+
+        Arguments:
+            * sample_size: size of the samples drawn
+            * sim_reps: number of times samples are drawn
+        """
+        population = self.sampler(100000)
+        t_list = np.empty(sim_reps)
+
+        for i in range(sim_reps):
+            current_sample = self.sampler(sample_size)
+            t_list[i] = ((np.mean(current_sample) - np.mean(population)) /
+                         (np.std(current_sample, ddof=1) / np.sqrt(sample_size)))
+
+        return t_list, sample_size
+
+
+    def empirical_cdf(self, data):
+        """
+        Returns sorted data and the corresponding percentiles as a tuple.
+        """
+        data.sort()
+        percentile = np.ones_like(data).cumsum() / (len(data) + 1)
+        return data, percentile
+
+
+    def plot_dist_npp(self, sampling_mean_output):
+        """
+        Generates the following two plots:
+            1. Theoretical vs sampling distribution of standardized mean
+            2. Normal probability plot
+        """
+        t_list, sample_size = sampling_mean_output
+
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 5))
+        # Left plot
+        t_grid = np.linspace(-3.5, 3.5, len(t_list))
+        t_dist = stats.t.pdf(t_grid, df=sample_size-2)
+        ax1.plot(t_grid, t_dist, c='r',
+                 label='Theoretical t-distribution with df={}'.format(sample_size-2))
+        sns.distplot(t_list, norm_hist=True, hist=False, kde=True, kde_kws={'color':'orange'},
+                     label='Sampling distribution of standardized mean', ax=ax1)
+        ax1.legend(loc=9, fontsize=12)
+        ax1.set_title('Theoretical vs Sampling Distribution of Standardized Mean', fontsize=15)
+        ax1.set_ylim(0, 0.5)
+        ax1.set_xlim(-3.5, 3.5)
+        ax1.text(1.5, 0.35, 'KL Divergence:')
+        ax1.text(1.5, 0.325,
+                 np.round(stats.entropy(stats.gaussian_kde(t_list).pdf(t_grid), t_dist), 4))
+
+        # Right plot
+        sorted_t_list, percentile = self.empirical_cdf(t_list)
+        theo_q = stats.t.ppf(percentile, df=sample_size-2)
+        ax2.scatter(theo_q, sorted_t_list, c='lightblue', s=10)
+        # Add x=y line for visual comparison
+        sns.regplot(np.array([0, 1]), np.array([0, 1]), ax=ax2, ci=None, scatter=False,
+                    line_kws={'color':'grey', 'lw':1})
+        ax2.set_title('Normal Probability Plot', fontsize=15)
+        ax2.set_xlabel('Theoretical quantiles')
+        ax2.set_ylabel('Observed t-statistics')
+
+
+    def find_alpha(self, sampling_mean_output, desired_alpha):
+        """
+        Given a desired alpha, computes the actual alpha (for a two-tailed test) from the sampling
+        distribution and prints it.
+       """
+        t_list, sample_size = sampling_mean_output
+        sorted_t_list, percentile = self.empirical_cdf(t_list)
+
+        desired_t = stats.t.ppf(desired_alpha/2, df=sample_size-2)
+        i_lower = np.searchsorted(sorted_t_list, desired_t, side='left')
+        i_upper = np.searchsorted(sorted_t_list, -desired_t, side='right')
+
+        print('Desired alpha for a two-tailed test:', desired_alpha)
+        print('Actual alpha for a two-tailed test:', 
+              percentile[i_lower] + (1 - percentile[i_upper]))
+
+        # Another approach:
+        '''
+        n = np.size(t_list[t_list < desired_t]) + np.size(t_list[t_list > -desired_t])
+        n /= len(t_list)
+        print('Actual alpha for a two-tailed test:', n)
+        '''
+
+
+    def report(self, sample_size, desired_alpha, sim_reps=10000):
+        """
+        Runs all the functions and outputs the following:
+            * Two plots:
+                1. The pdf (by KDE) of the sampling distribution of the t-statistics along with
+                   the theoretical t-distribution
+	        2. The normal probability plot of the sample t-statistics
+            * Actual test size α from the sampling distribution based on the critical values for
+              the desired α (for a two-tailed test)
+
+        Arguments:
+            * sample_size: size of the samples drawn
+            * desired_alpha: desired α for a two-tailed test
+            * sim_reps: number of times samples are drawn
+        """
+        sampling_distribution = self.sampling_mean(sample_size, sim_reps)
+        self.plot_dist_npp(sampling_distribution)
+        self.find_alpha(sampling_distribution, desired_alpha)
